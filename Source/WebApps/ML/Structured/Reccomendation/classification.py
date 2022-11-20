7a0e953ed8241394a9678228c0b9812db2e490d8
Randy <randomzakya@gmail.com>
2022-11-20 11:08:14 +0700

    add clf svm

--- Source/WebApps/ML/Structured/Reccomendation/classification.py
+++ Source/WebApps/ML/Structured/Reccomendation/classification.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
 from xgboost import XGBClassifier
 
 def xgboost(df,columns,target,**kwargs):
@@ -14,3 +15,10 @@ def adaboost(df,columns,target,**kwargs):
     ada.fit(df_[columns],df_[target])
     df_["prediction"] = ada.predict(df_[columns])
     return df_,ada
+
+def svm_classifier(df,columns,target,**kwargs):
+    df_ = df.copy()
+    svm = SVC(**kwargs)
+    svm.fit(df_[columns],df_[target])
+    df_["prediction"] = svm.predict(df_[columns])
+    return df_,svm
