7df5a3488dd2a1ba44cd50f871e97b3ed53487ad
Randy <randomzakya@gmail.com>
2022-11-20 16:04:29 +0700

    del feat select

--- Source/WebApps/ML/Text/Clustering/cluster.py
+++ Source/WebApps/ML/Text/Clustering/cluster.py
@@ -1,14 +1,12 @@
 from sklearn.cluster import AgglomerativeClustering
 from sklearn_extra.cluster import KMedoids
 
-def kmedoids(df,columns,n_clusters,**kwargs):
-    df_ = df.copy()
+def kmedoids(vectors,n_clusters,**kwargs):
     kmedoids = KMedoids(n_clusters=n_clusters,metric="cosine",**kwargs)
-    df_["cluster"] = kmedoids.fit_predict(df_[columns])
-    return df_,kmedoids
+    clusters = kmedoids.fit_predict(vectors)
+    return clusters,kmedoids
 
-def agglo_cluster(df,columns,n_clusters,**kwargs):
-    df_ = df.copy()
-    agglo = AgglomerativeClustering(n_clusters=n_clusters,linkage="average",affinity="cosine",**kwargs)
-    df_["cluster"] = agglo.fit_predict(df_[columns])
-    return df_,agglo
+def agglo_cluster(vectors,n_clusters,**kwargs):
+    agglo = AgglomerativeClustering(n_clusters=n_clusters,linkage="average",affinity="cosine")
+    clusters = agglo.fit_predict(vectors)
+    return clusters,agglo
